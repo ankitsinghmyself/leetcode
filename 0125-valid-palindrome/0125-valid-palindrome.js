7e32df54b04739831c12cbecc2f5699531d4441b
Ankit Singh <43289790+ankitsinghmyself@users.noreply.github.com>
2026-02-19 22:06:51 +0530

    Time: 6 ms (51.29%), Space: 57.7 MB (65.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,6 +3,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let trimString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
-    return trimString === trimString.split('').reverse().join('')
+    let trimString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let start = 0;
+    let end = trimString.length-1;
+    while(start < end){
+        if(trimString[start]===trimString[end]){
+            start++;
+            end--;
+        }else{
+            return false
+        }
+    }
+    return true
 };
